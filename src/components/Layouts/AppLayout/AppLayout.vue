74c4efd8566ef24568152bdc23899f2fd67a8d6d
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-11-08 12:26:53 -0300

    :lipstick: feat: estilização de layout do app

--- src/components/Layouts/AppLayout/AppLayout.vue
+++ src/components/Layouts/AppLayout/AppLayout.vue
@@ -1,7 +1,13 @@
+<script setup>
+import Sidebar from '../../Sidebar/Sidebar.vue';
+</script>
+
 <template>
     <div>
-        <main>
-            <router-view></router-view>
+        <Sidebar />
+        <main class="p-4 sm:ml-64">
+            <h1>teste</h1>
+            <!-- <router-view></router-view> -->
         </main>
     </div>
 </template>
