4226dedb5e595ce5731ebc287ffbd0bea36d7f71
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-11-13 15:05:14 -0300

    :wrench: chore: config dos ícones e de componentes globais

--- src/main.js
+++ src/main.js
@@ -4,7 +4,19 @@ import App from './App.vue';
 import router from './router/index.js';
 import store from './store';
 
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { CoClock, BiPlusCircle, BiArrowRight, BiArrowDown, 
+    BiCircle, BiCheck2Circle, BiArrowDownUp, BiArrowUp } 
+from "oh-vue-icons/icons";
+
+addIcons(
+    CoClock, BiPlusCircle, BiArrowRight, BiArrowDown,BiArrowUp,
+    BiCircle, BiCheck2Circle, BiArrowDownUp
+);
+
 const app = createApp(App);
+
+app.component("v-icon", OhVueIcon);
 
 app
 .use(store)
