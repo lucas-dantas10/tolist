bf7a7e2c28dea91ac33f3caf74c960dc705383d3
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-11-13 15:09:14 -0300

    :sparkles: feat: adicionado se a requisição der tudo certo, para setar usuario e token com o commit

--- src/store/actions/login/login.js
+++ src/store/actions/login/login.js
@@ -1,6 +1,12 @@
-import axios from 'axios';
+import axiosClient from '../../../axios';
 
 export function login({commit}, user) {
-    return axios.post('http://localhost:8080/api/login', user);
+    return axiosClient.post('http://localhost:8080/api/login', user)
+        .then(({data}) => {
+            console.log(data);
+            commit('setUser', data.user);
+            commit('setToken', data.token);
+            return data;
+        });
 }
 
--- src/store/mutation.js
+++ src/store/mutation.js
@@ -1,6 +1,9 @@
 import { hideToast, showToast } from "./mutations/toast/toast";
+import { setUser, setToken } from "./mutations/user/user";
 
 export default {
     hideToast,
     showToast,
+    setUser,
+    setToken,
 }
