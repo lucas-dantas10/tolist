1daea69f0bfbad32eef2091f18b03876ccc71aa0
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-11-14 14:25:08 -0300

    :lipstick: feat: adicionado ícones para o sidebar

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -1,12 +1,12 @@
 <script setup>
-import { onBeforeUnmount, onMounted, ref } from 'vue';
+import { ref } from 'vue';
 
 const menus = ref([
-    { name: 'Tarefas', link: '/home' },
-    { name: 'Cadastro Tarefas', link: '/' },
-    { name: 'Cadastro Categoria', link: '/' },
-    { name: 'Configurações', link: '/' },
-    { name: 'Sair', link: '/' },
+    { name: 'Tarefas', link: '/home', icon: 'fa-tasks', color: 'blue' },
+    { name: 'Cadastro Tarefas', link: '/', icon: 'la-clipboard-list-solid', color: 'green' },
+    { name: 'Cadastro Categoria', link: '/', icon: 'bi-list-stars', color: 'yellow' },
+    { name: 'Configurações', link: '/', icon: 'ri-settings-4-line', color: 'white' },
+    { name: 'Sair', link: '/', icon: 'bi-arrow-bar-left', color: 'red' },
 ])
 
 const isOpen = ref(false);
@@ -29,7 +29,6 @@ function handleOutsideClick(event) {
         isOpen.value = false;
         document.removeEventListener('click', handleOutsideClick);
     }
-    
 }
 </script>
 
@@ -65,29 +64,16 @@ function handleOutsideClick(event) {
         aria-label="Sidebar"
         :class="{ 'translate-x-[0]': isOpen }"
     >
-        <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+        <div class="h-full px-3 py-4 overflow-y-auto bg-gray-900">
             <ul class="space-y-2 font-medium">
                 <li v-for="(menu, i) in menus" :key="i">
-                    <a
-                        :href="menu.link"
+                    <router-link
+                        :to="menu.link"
                         class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                     >
-                        <svg
-                            class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="currentColor"
-                            viewBox="0 0 22 21"
-                        >
-                            <path
-                                d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"
-                            />
-                            <path
-                                d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"
-                            />
-                        </svg>
+                        <v-icon :name="menu.icon" :fill="menu.color" />
                         <span class="ml-3">{{ menu.name }}</span>
-                    </a>
+                    </router-link>
                 </li>
             </ul>
         </div>
