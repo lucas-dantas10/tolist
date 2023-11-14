7196f071b27f259d0913dfcd9e729dbd53d35248
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-11-14 14:22:15 -0300

    :lipstick: feat: estilização de dropdown comum

---
+++ src/components/Dropdown/Dropdown.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const dropdown = ref(null);
+const props = defineProps({
+    icon: String,
+    items: Array,
+})
+
+onMounted(() => {
+    dropdown.value.focus();
+});
+
+function openDropdown() {
+    dropdown.value.classList.toggle("hidden");
+}
+
+function actionTask(item) {
+    if (item.title == 'Deletar') {
+        if (!confirm('Você deseja deletar esta tarefa?')) {
+            return;
+        }
+
+        console.log('deletado')
+        return;
+    }
+}
+</script>
+
+<template>
+    <button
+        id="dropdownDefaultButton"
+        data-dropdown-toggle="dropdown"
+        @click="openDropdown()"
+        class="border border-gray-200 rounded-lg px-2 py-1 hover:bg-gray-300 hover:cursor-pointer"
+        type="button"
+    >
+        <v-icon :name="props.icon" />
+    </button>
+
+    <!-- Dropdown menu -->
+    <div
+        id="dropdown"
+        ref="dropdown"
+        class="z-10 hidden mt-2 mr-8 fixed bg-white divide-y divide-gray-100 rounded-lg shadow w-20"
+    >
+        <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+            <li v-for="(item, i) in props.items" :key="i">
+                <a href="#" @click="actionTask(item)" class="block px-4 py-2 hover:bg-gray-100 text-black"
+                    >{{item.title}}</a
+                >
+            </li>
+        </ul>
+    </div>
+</template>
