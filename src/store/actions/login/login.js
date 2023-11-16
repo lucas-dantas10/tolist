ebd2138111fb6899019cfe4cc008756fed1c95f5
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-11-16 09:00:08 -0300

    :sparkles: feat: adicionado commit para setar usuario no state e setar token

--- src/store/actions/login/login.js
+++ src/store/actions/login/login.js
@@ -3,7 +3,6 @@ import axiosClient from '../../../axios';
 export function login({commit}, user) {
     return axiosClient.post('/login', user)
         .then(({data}) => {
-            console.log(data);
             commit('setUser', data.user);
             commit('setToken', data.token);
             return data;
