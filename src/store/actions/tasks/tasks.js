346e46d9971ef8111e8eae17fe97583c8e9df0e2
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-11-20 18:50:56 -0300

    :sparkles: feat: adicionado parametros para busca de tarefas, podendo ocorrer mudanças nos parametros

--- src/store/actions/tasks/tasks.js
+++ src/store/actions/tasks/tasks.js
@@ -2,9 +2,7 @@ import axiosClient from '../../../axios';
 
 export function getTasks({commit}, { url = null, search = '', per_page = 10, sort_field = 'title', sort_direction = 'asc', status, priority } = {}) {
 
-    url = url || '/task'; 
-
-    console.log(status);
+    url = url || '/task';
 
     return axiosClient.get(url, {
         params: {
