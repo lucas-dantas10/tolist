7126e3d16ca92c51b325e3757c8cf34d9ba0821a
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-11-20 18:46:59 -0300

    :sparkles: feat: adicionada lógica para contar a quantidade de cada tarefas com determinado status

--- src/store/mutations/status/status.js
+++ src/store/mutations/status/status.js
@@ -1,3 +1,27 @@
 export function setStatus(state, allStatus) {
     state.status.data = allStatus;
+
+    const statusCounts = {
+        'Fazer': 0,
+        'Em progresso': 0,
+        'Feito': 0,
+        'Cancelado': 0,
+    };
+
+    state.tasks.data.forEach(task => {
+        const statusType = task.status.type;
+        if (statusType in statusCounts) {
+            statusCounts[statusType] += 1;
+        } else {
+            console.warn(`Unknown task status: ${statusType}`);
+        }
+    });
+
+    state.status.data.forEach(status => {
+        status.amountToDo = statusCounts["Fazer"];
+        status.amountInProgress = statusCounts["Em progresso"];
+        status.amountDone = statusCounts["Feito"];
+        status.amountCanceled = statusCounts["Cancelado"];
+        status.isCheck = false;
+    });  
 }
