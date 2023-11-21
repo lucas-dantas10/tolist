9c45d1b55df8757d3e76c36555676309bfeae2b7
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-11-21 16:15:13 -0300

    :construction: build: Em constante mudança, adicionada funcionalidades dos selects e das views

--- src/views/Home/TasksTable.vue
+++ src/views/Home/TasksTable.vue
@@ -7,6 +7,8 @@ import store from '../../store';
 const tasks = computed(() => store.state.tasks.data);
 const status = computed(() => store.state.status.data);
 const priorities = computed(() => store.state.priority.data);
+const views = ref([{type: 'Titulo'}, {type: 'Status'}, { type: 'Prioridade' }]);
+const isViewActive = ref({title: true, status: true, priority: true});
 
 const search = ref('');
 const perPage = ref(10);
@@ -54,30 +56,75 @@ function filter(item) {
         return;
     }
 
+    switch (item.type) {
+        case "Titulo":
+            isViewActive.value.title = !isViewActive.value.title;
+            break;
+        case "Status":
+            isViewActive.value.status = !isViewActive.value.status;
+            break;
+        case "Prioridade":
+            isViewActive.value.priority = !isViewActive.value.priority;
+            break;
+    }
+
     statusId.value = 0;
     priorityId.value = 0;
     getTasks();
     
 }
 
+function deleteTask(action) { 
+    action.type = action.type.toLowerCase();
+    if (action.type == 'deletar') {
+        if (!confirm('Você deseja deletar esta tarefa?')) {
+            return;
+        }
+        store.dispatch('deleteTask', action.task.id);
+        getTasks();
+        return;
+    }
+
+    // if (action.type == 'editar') {
+    //     store.dispatch('editarTask', action.task.id);
+    // }
+    
+}
+
+function reset() {
+    search.value = '';
+    perPage.value = 10;
+    sortField.value = 'title';
+    sortDirection.value = 'asc';
+    priorityId.value = 0;
+    statusId.value = 0;
+    getTasks();
+}
+
 </script>
 
 <template>
     <div>
-        <form @submit.prevent="filter()" class="flex flex-col items-start gap-2 mb-8 lg:flex-row lg:items-center lg:justify-between">
+        <form @submit.prevent="getTasks()" class="flex flex-col items-start gap-2 mb-8 lg:flex-row lg:items-center lg:justify-between">
             <div class="flex flex-col items-start gap-2 lg:flex-row lg:items-center">
                 <input
                     type="text"
                     placeholder="Filtro tarefas..."
+                    v-model="search"
                     class="border border-gray-200 rounded-lg px-2 py-1 focus:outline-gray-200"
                 />
 
                 <DropdownSearch title="Status" icon="bi-plus-circle" :items="status" @checked="filter" />
 
                 <DropdownSearch title="Prioridade" icon="bi-plus-circle" :items="priorities" @checked="filter" />
+
+                <button v-if="search != ''" type="button" @click="reset()" class="font-semibold px-4 py-1 hover:border hover:bg-gray-100 hover:rounded-md transition-colors">
+                    Reset
+                    <v-icon name="io-close-outline" />
+                </button>
             </div>
 
-            <DropdownSearch title="View" icon="bi-arrow-down-up" margin-inline="-9.5" :items="[{title: 'Title', model: 'progress'}, {title: 'Status'}]" />
+            <DropdownSearch title="View" icon="bi-arrow-down-up" margin-inline="-9" :items="views" @checked="filter" />
 
             <button
                 type="submit"
@@ -92,9 +139,9 @@ function filter(item) {
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50">
                     <tr>
                         <th scope="col" class="px-6 py-3">Tarefa</th>
-                        <th scope="col" class="px-6 py-3">Title</th>
-                        <th scope="col" class="px-6 py-3">Status</th>
-                        <th scope="col" class="px-6 py-3">Prioridade</th>
+                        <th v-if="isViewActive.title" scope="col" class="px-6 py-3">Titulo</th>
+                        <th v-if="isViewActive.status" scope="col" class="px-6 py-3">Status</th>
+                        <th v-if="isViewActive.priority" scope="col" class="px-6 py-3">Prioridade</th>
                         <th scope="col" class="px-6 py-3">Ações</th>
                     </tr>
                 </thead>
@@ -113,20 +160,20 @@ function filter(item) {
                                 {{task.title}}
                             </div>                            
                         </th>
-                        <td class="px-6 py-4">
+                        <td class="px-6 py-4" v-if="isViewActive.title">
                             <span class="border border-gray-200 px-2 py-1 rounded-md mr-2">{{task.category.name}}</span>
                             {{task.description}}
                         </td>
-                        <td class="px-6 py-4">
+                        <td class="px-6 py-4" v-if="isViewActive.status">
                             <v-icon :name="task.status.icon" fill="black" />
                             {{task.status.type}}
                         </td>
-                        <td class="px-6 py-4">
+                        <td class="px-6 py-4" v-if="isViewActive.priority">
                             <v-icon :name="task.priority.icon" fill="black" />
                             {{task.priority.type}}
                         </td>
                         <td class="px-6 py-4">
-                            <Dropdown icon="bi-three-dots" :items="[{title: 'Editar'}, {title: 'Deletar'}]" />
+                            <Dropdown icon="bi-three-dots" :items="[{title: 'Editar', task: task}, {title: 'Deletar', task: task}]" @action="deleteTask" />
                         </td>
                     </tr>
                 </tbody>
