ddcf497a7c59ca83c89fa6f5c680d16e02327973
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-11-22 16:25:33 -0300

    :sparkles: feat: adicionada lógica de contador de tarefas com determinada prioridade

--- src/store/mutations/priority/priority.js
+++ src/store/mutations/priority/priority.js
@@ -1,4 +1,25 @@
 export function setPriority(state, priorities) {
     state.priority.data = priorities;
-    state.priority.data.forEach(priority => priority.isCheck = false);
+
+    const priorityCounts = {
+        "Alta": 0,
+        "Média": 0,
+        "Baixa": 0,
+    }
+
+    state.tasks.data.forEach(task => {
+        const priorityType = task.priority.type;
+        if (priorityType in priorityCounts) {
+            priorityCounts[priorityType] += 1;
+        } else {
+            console.warn(`Unknown task status: ${priorityType}`);
+        }
+    });
+
+    state.priority.data.forEach(priority => {
+        priority.amountHigh = priorityCounts["Alta"];
+        priority.amountAverage = priorityCounts["Média"];
+        priority.amountLow = priorityCounts["Baixa"];
+        priority.isCheck = false
+    });
 }
