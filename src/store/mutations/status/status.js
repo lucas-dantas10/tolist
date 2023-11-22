61a764e0645fd646be805cbab585fa880c041b79
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-11-22 16:23:12 -0300

    :recycle: refactor: mudança no nome dos objetos e lógica de contador de quantidade de status

--- src/store/mutations/status/status.js
+++ src/store/mutations/status/status.js
@@ -3,15 +3,17 @@ export function setStatus(state, allStatus) {
 
     const statusCounts = {
         'Fazer': 0,
-        'Em progresso': 0,
+        'progresso': 0,
         'Feito': 0,
         'Cancelado': 0,
     };
 
     state.tasks.data.forEach(task => {
-        const statusType = task.status.type;
+        let statusType = task.status.type;
+        statusType == 'Em progresso' ? statusType = 'progresso' : '';
         if (statusType in statusCounts) {
             statusCounts[statusType] += 1;
+            
         } else {
             console.warn(`Unknown task status: ${statusType}`);
         }
@@ -19,7 +21,7 @@ export function setStatus(state, allStatus) {
 
     state.status.data.forEach(status => {
         status.amountToDo = statusCounts["Fazer"];
-        status.amountInProgress = statusCounts["Em progresso"];
+        status.amountInProgress = statusCounts["progresso"];
         status.amountDone = statusCounts["Feito"];
         status.amountCanceled = statusCounts["Cancelado"];
         status.isCheck = false;
