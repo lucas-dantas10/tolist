56adfcb284d71a20169720ffd8566d8053cd8c6c
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-11-24 16:43:19 -0300

    :sparkles: feat: adicioando setTimeout para dar o tempo do lifecycle do vue e carregar os valores necessários

--- src/store/mutations/priority/priority.js
+++ src/store/mutations/priority/priority.js
@@ -6,20 +6,21 @@ export function setPriority(state, priorities) {
         "Média": 0,
         "Baixa": 0,
     }
-
-    state.tasks.data.forEach(task => {
-        const priorityType = task.priority.type;
-        if (priorityType in priorityCounts) {
-            priorityCounts[priorityType] += 1;
-        } else {
-            console.warn(`Unknown task status: ${priorityType}`);
-        }
-    });
-
-    state.priority.data.forEach(priority => {
-        priority.amountHigh = priorityCounts["Alta"];
-        priority.amountAverage = priorityCounts["Média"];
-        priority.amountLow = priorityCounts["Baixa"];
-        priority.isCheck = false
-    });
+    setTimeout(() => {
+        state.tasks.data.forEach(task => {
+            const priorityType = task.priority.type;
+            if (priorityType in priorityCounts) {
+                priorityCounts[priorityType] += 1;
+            } else {
+                console.warn(`Unknown task status: ${priorityType}`);
+            }
+        });
+    
+        state.priority.data.forEach(priority => {
+            priority.amountHigh = priorityCounts["Alta"];
+            priority.amountAverage = priorityCounts["Média"];
+            priority.amountLow = priorityCounts["Baixa"];
+            priority.isCheck = false
+        });
+    }, 0);
 }
--- src/store/mutations/tasks/tasks.js
+++ src/store/mutations/tasks/tasks.js
@@ -1,3 +1,16 @@
-export function setTasks(state, tasks) {
-    state.tasks.data = tasks;
+export function setTasks(state, [loading, data = null]) {
+    if (data) {
+        state.tasks = {
+            ...state.tasks,
+            data: data.data,
+            links: data.meta?.links,
+            page: data.meta.current_page,
+            limit: data.meta.per_page,
+            from: data.meta.from,
+            to: data.meta.to,
+            total: data.meta.total,
+        };
+    }
+
+    state.tasks.loading = loading;
 }
