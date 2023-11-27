ea6057a732614f8e2f67cc4a3ab47ebd4e1b4ac5
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-11-27 15:10:33 -0300

    :sparkles: feat: formulario para criar tarefa finalizado e funcional

--- src/views/RegisterTask/RegisterTask.vue
+++ src/views/RegisterTask/RegisterTask.vue
@@ -1,5 +1,73 @@
+<script setup>
+import { computed, ref } from 'vue';
+import store from '../../store';
+import BaseInput from '../../components/BaseInput/BaseInput.vue';
+
+const form = ref({});
+const categories = computed(() => store.state.category);
+
+function registerTask() {
+    store.dispatch('storeTask', form.value);
+}
+</script>
+
 <template>
     <section>
         <h1 class="text-4xl text-gray-900 font-bold">Cadastro Tarefa</h1>
+
+        <form class="mt-8" @submit.prevent="registerTask">
+            <div class="flex flex-col gap-2">
+                <BaseInput 
+                    v-model="form.title"
+                    label="Título"
+                />
+
+                <BaseInput 
+                    v-model="form.description"
+                    label="Descrição"
+                />
+
+                <div class="flex flex-col gap-2 justify-center">
+                    <label class="text-xl font-semibold">Categoria</label>
+                    <select v-model="form.category" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-dotted block w-3/6 py-2 px-2">
+                        <option selected>Escolha uma Categoria</option>
+                        <option v-for="category in categories.data" :key="category.id" :value="category.id">
+                            {{ category.name }}
+                        </option>
+                    </select>
+                </div>
+                <div class="flex flex-col gap-2 justify-center">
+                    <label class="text-xl font-semibold">Status</label>
+                    <select v-model.number="form.status" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-dotted block w-3/6 py-2 px-2">
+                        <option selected>Escolha um Status</option>
+                        <option value="1">Fazer</option>
+                        <option value="3">Feito</option>
+                        <option value="2">Em progresso</option>
+                        <option value="4">Cancelado</option>
+                    </select>
+                </div>
+                <div class="flex flex-col gap-2 justify-center">
+                    <label class="text-xl font-semibold">Prioridade</label>
+                    <select v-model.number="form.priority" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-dotted block w-3/6 py-2 px-2">
+                        <option selected>Escolha uma Prioridade</option>
+                        <option value="1">Alta</option>
+                        <option value="3">Média</option>
+                        <option value="2">Baixa</option>
+                    </select>
+                </div>
+                <div class="flex flex-col gap-2 justify-center">
+                    <label class="text-xl font-semibold">Esquema</label>
+                    <select v-model.number="form.schedule" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-dotted block w-3/6 py-2 px-2">
+                        <option selected>Escolha um Esquema</option>
+                        <option value="1">Segunda a Sexta</option>
+                        <option value="2">Todos os dias</option>
+                    </select>
+                </div>
+            </div>
+
+            <button type="submit" class="mt-6 border border-blue-600 bg-blue-600 text-white px-4 py-1 rounded-md">
+                Cadastrar
+            </button>
+        </form>
     </section>
 </template>
