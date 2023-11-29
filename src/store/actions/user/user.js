fb8c4341399cd4cd5eb4cfb1acd9562060dc991f
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-11-29 14:30:07 -0300

    :sparkles: feat: adicionada função para cadastrar usuário

--- src/store/actions/user/user.js
+++ src/store/actions/user/user.js
@@ -4,3 +4,26 @@ export function getCurrentUser({commit}) {
     return axiosClient.get('/current-user')
         .then(({data}) => commit('setUser', data));
 }
+
+export function storeUser({commit, state}, user) {
+    state.spinner.loading = true;
+    return axiosClient.post('/user', user)
+        .then(({data}) => {
+            commit('showToast', {
+                message: data.message,
+                type: 'success',
+            });
+
+            return data;
+        })
+        .catch(({response}) => {
+            console.log(response);
+            commit('showToast', {
+                message: response.data.message,
+                type: 'error',
+            });
+
+            return response;
+        })
+        .finally(() => state.spinner.loading = false);
+}
