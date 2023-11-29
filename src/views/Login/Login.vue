26e0080c8003ebfb44a0105795c776349dabac46
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-11-29 14:32:03 -0300

    :lipstick: style: Mudança no layout de login e register para light mode principal

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -31,14 +31,14 @@ function submit() {
 </script>
 
 <template>
-    <section class="bg-gray-50 dark:bg-gray-900 h-screen">
+    <section class="bg-gray-50">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
             <div
-                class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+                class="w-full bg-gray-300 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0"
             >
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                     <h1
-                        class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
+                        class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl"
                     >
                         Entrar com a sua conta
                     </h1>
@@ -52,7 +52,7 @@ function submit() {
                     </div>
                     <form @submit.prevent="submit" class="space-y-4 md:space-y-6">
                         <div>
-                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900"
                                 >Seu Email</label
                             >
                             <input
@@ -60,7 +60,7 @@ function submit() {
                                 name="email"
                                 id="email"
                                 v-model.trim="forms.email"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                                 placeholder="name@company.com"
                                 required=""
                             />
@@ -68,7 +68,7 @@ function submit() {
                         <div>
                             <label
                                 for="password"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                class="block mb-2 text-sm font-medium text-gray-900"
                                 >Senha</label
                             >
                             <input
@@ -77,7 +77,7 @@ function submit() {
                                 id="password"
                                 v-model.trim="forms.password"
                                 placeholder="••••••••"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                                 required=""
                             />
                         </div>
@@ -89,31 +89,31 @@ function submit() {
                                         aria-describedby="remember"
                                         type="checkbox"
                                         v-model="forms.remember"
-                                        class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                                        class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
                                     />
                                 </div>
                                 <div class="ml-3 text-sm">
-                                    <label for="remember" class="text-gray-500 dark:text-gray-300"
+                                    <label for="remember" class="text-gray-500"
                                         >Lembre de mim</label
                                     >
                                 </div>
                             </div>
-                            <a
+                            <!-- <a
                                 href="#"
-                                class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                                class="text-sm font-medium text-primary-600 hover:underline"
                                 >Esqueceu a senha?</a
-                            >
+                            > -->
                         </div>
                         <button
                             type="submit"
-                            class="w-full flex items-center justify-center gap-2 text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                            class="w-full flex items-center justify-center gap-2 text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                         >
                             {{ spinner.loading == false ? 'Entrar' : 'Verificando...' }}
                             <Spinner />
                         </button>
-                        <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                        <p class="text-sm font-light text-gray-500">
                             Você não tem conta ainda?
-                            <router-link to="register" class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                            <router-link to="register" class="font-medium text-primary-600 hover:underline"
                                 >Registrar</router-link>
                         </p>
                     </form>
--- src/views/Register/Register.vue
+++ src/views/Register/Register.vue
@@ -1,85 +1,122 @@
+<script setup>
+import { ref } from 'vue';
+import store from '../../store';
+import Spinner from '../../components/Spinner/Spinner.vue';
+import router from '../../router';
+
+const form = ref({});
+
+function registerUser() {
+    store.dispatch('storeUser', form.value)
+        .then((data) => {
+            if (data.status == 200) {
+                form.value = {};
+                router.push({ name: login });
+            }             
+        })
+        .catch(() => console.log('erro'));
+}
+</script>
+
 <template>
-    <section class="bg-gray-50 dark:bg-gray-900">
+    <section class="bg-gray-50">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
             <div
-                class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+                class="w-full bg-gray-300 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0"
             >
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                     <h1
-                        class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
+                        class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl"
                     >
                         Criar uma Conta
                     </h1>
-                    <form class="space-y-4 md:space-y-6" action="#">
+                    <form @submit.prevent="registerUser" class="space-y-4 md:space-y-6" action="#">
                         <div>
-                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900"
+                                >Seu Nome</label
+                            >
+                            <input
+                                type="text"
+                                name="name"
+                                id="name"
+                                v-model.trim="form.name"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                placeholder="name"
+                            />
+                        </div>
+                        <div>
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900"
                                 >Seu Email</label
                             >
                             <input
                                 type="email"
                                 name="email"
                                 id="email"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                v-model.trim="form.email"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                                 placeholder="name@company.com"
                             />
                         </div>
                         <div>
                             <label
                                 for="password"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                class="block mb-2 text-sm font-medium text-gray-900"
                                 >Senha</label
                             >
                             <input
                                 type="password"
                                 name="password"
                                 id="password"
+                                v-model.trim="form.password"
                                 placeholder="••••••••"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                             />
                         </div>
                         <div>
                             <label
                                 for="confirm-password"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                class="block mb-2 text-sm font-medium text-gray-900"
                                 >Confirmar Senha</label
                             >
                             <input
-                                type="confirm-password"
+                                type="password"
                                 name="confirm-password"
                                 id="confirm-password"
+                                v-model.trim="form.confirmPassword"
                                 placeholder="••••••••"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                             />
                         </div>
-                        <div class="flex items-start">
+                        <!-- <div class="flex items-start">
                             <div class="flex items-center h-5">
                                 <input
                                     id="terms"
                                     aria-describedby="terms"
                                     type="checkbox"
-                                    class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                                    class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
                                 />
                             </div>
                             <div class="ml-3 text-sm">
-                                <label for="terms" class="font-light text-gray-500 dark:text-gray-300"
+                                <label for="terms" class="font-light text-gray-500"
                                     >Eu aceito os
                                     <a
-                                        class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                                        class="font-medium text-primary-600 hover:underline"
                                         href="#"
                                         >Termos e Condições</a
                                     ></label
                                 >
                             </div>
-                        </div>
+                        </div> -->
                         <button
                             type="submit"
-                            class="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                            class="w-full flex items-center justify-center gap-2 text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600"
                         >
                             Criar uma conta
+                            <Spinner />
                         </button>
-                        <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                        <p class="text-sm font-light text-gray-500">
                             Já possui uma conta?
-                            <router-link to="/login" class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                            <router-link to="/login" class="font-medium text-primary-600 hover:underline"
                                 >Entrar aqui</router-link
                             >
                         </p>
