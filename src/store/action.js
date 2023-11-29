d3ed82b8a42dd359d113344b85cd656479d2a4e7
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-11-29 14:30:40 -0300

    :sparkles: feat: adicionada a função de cadastro  usuario para poder ser utilizada

--- src/store/action.js
+++ src/store/action.js
@@ -1,6 +1,6 @@
 import { login, logout } from "./actions/login/login";
 import { getTasks, deleteTask, updateTask, storeTask } from "./actions/tasks/tasks";
-import { getCurrentUser } from "./actions/user/user";
+import { getCurrentUser, storeUser } from "./actions/user/user";
 import { getStatus } from "./actions/status/status";
 import { getPriority } from "./actions/priority/priority";
 import { getCategories, storeCategory } from "./actions/category/category";
@@ -17,4 +17,5 @@ export default {
     logout,
     storeTask,
     storeCategory,
+    storeUser,
 }
