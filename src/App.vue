df69ab8e12f12c702676c3477bf80013442a7a7d
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-11-30 10:57:46 -0300

    :sparkles: feat: adicionada funcionalidade de darkMode

--- src/App.vue
+++ src/App.vue
@@ -9,3 +9,13 @@ import Toast from './components/Toast/Toast.vue';
         <router-view></router-view>
     </div>
 </template>
+
+<style>
+html.dark {
+    color-scheme: dark;
+}
+
+body {
+    @apply bg-white transition-colors delay-300 dark:bg-gray-900
+}
+</style>
