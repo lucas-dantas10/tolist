f99bb6d2a1b147a8047d27c638833b6efc2e6e06
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-12-02 20:41:28 -0300

    :sparkles: feat: Adicionado prop de 'required' para os inputs

--- src/components/BaseInput/BaseInput.vue
+++ src/components/BaseInput/BaseInput.vue
@@ -14,6 +14,9 @@ const props = defineProps({
     },
     selectOptions: {
         type: Array
+    },
+    required: {
+        type: Boolean
     }
 });
 </script>
@@ -25,6 +28,7 @@ const props = defineProps({
             :placeholder="label"
             :value="modelValue"
             :type="type"
+            :required="required"
             @input="$emit('update:modelValue', $event.target.value)"
             class="border border-gray-300 rounded-md w-3/6 px-2 py-1 focus:outline-dotted dark:bg-gray-200 dark:text-black"
         />
